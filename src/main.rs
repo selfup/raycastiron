cb956be3b6567288069ecca8c33067de8d66140f
Matt Pindell <pindell.m@gmail.com>
2016-06-22 19:59:38 -0600

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,35 @@ mod player;
 mod map;
 
 fn main() {
-    let map = map::Map::new(3);
+    let mut map = map::Map::new(10);
     println!("{:?}", map);
+    println!("{:?}", map.randomize());
+    // println!("{:?}", map.get(2.0, 2.0));
 }
+
+
+pub fn ray(origin: u8) {
+
+}
+
+// function ray(origin) {
+//   var stepX = step(sin, cos, origin.x, origin.y);
+//   var stepY = step(cos, sin, origin.y, origin.x, true);
+//   var nextStep = stepX.length2 < stepY.length2
+//     ? inspect(stepX, 1, 0, origin.distance, stepX.y)
+//     : inspect(stepY, 0, 1, origin.distance, stepY.x);
+//
+//   if (nextStep.distance > range) return [origin];
+//   return [origin].concat(ray(nextStep));
+// }
+
+// function step(rise, run, x, y, inverted) {
+//          if (run === 0) return noWall;
+//          var dx = run > 0 ? Math.floor(x + 1) - x : Math.ceil(x - 1) - x;
+//          var dy = dx * (rise / run);
+//          return {
+//            x: inverted ? y + dy : x + dx,
+//            y: inverted ? x + dx : y + dy,
+//            length2: dx * dx + dy * dy
+//          };
+//        }
--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,7 @@
+extern crate rand;
+
+use self::rand::Rng;
+
 #[derive(Debug)]
 pub struct WallGrid {
     grid: Vec<u8>,
@@ -5,19 +9,48 @@ pub struct WallGrid {
 
 #[derive(Debug)]
 pub struct Map {
-    size: usize,
+    size: u8,
     wall_grid: WallGrid,
 }
 
 impl Map {
-    pub fn new(size: usize) -> Map {
-        let base_size = vec![0; size * size];
+    pub fn new(size: u8) -> Map {
+        let squared = usize::from(size * size);
+        let base_size = vec![0; squared];
 
         Map {
             size: size,
             wall_grid: WallGrid {
                 grid: base_size.clone(),
-            }
+            },
         }
+    }
+
+    pub fn get(self, x: f32, y: f32) -> i32 {
+        let x = x.floor();
+        let y = y.floor();
+
+        let current_size = f32::from(self.size);
+
+        if x < 0.0 || x > current_size - 1.0 || y < 0.0 || y > current_size - 1.0 {
+            let return_val: i32 = -1;
+            return_val
+        } else {
+            let index = y * current_size + x;
+            println!("{:?}", index);
+            // println!("{:?}", self.wall_grid.grid.position_elem(index));
+            // let return_val = self.wall_grid.grid[index];
+            123
+        }
+    }
+
+    pub fn randomize(&mut self) -> &mut Map {
+        let map_size = self.size * self.size;
+        for i in 0..map_size {
+            let num = rand::thread_rng().gen_range(0, 2);
+            let i = usize::from(i);
+            self.wall_grid.grid[i] = num;
+        };
+        self
     }
 }
